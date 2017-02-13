5bdffce96ec1a45b3ab504de0c2319621554f5b8
Hakon Hanesand <hakon@abello.no>
2017-02-12 19:37:01 -0600

    update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var session = require('express-session');
 var logger = require('morgan');
-
 var Grant = require('grant-express');
-var grant = new Grant(require('./config.json'));
 
 var app = express();
+var grant = new Grant(require('./config.json'));
+
 app.use(logger('dev'));
 app.use(session({secret: 'ffffffffff90501960cb3b29615931aabf74201b'}));
 app.use(grant);
