7bea6aeaa157de25711c5e28db1eb1e4da7853e0
Hakon Hanesand <hakon@abello.no>
2017-02-18 14:50:55 -0600

    Changes.

--- app.js
+++ app.js
@@ -1,18 +1,104 @@
-var express = require('express');
-var session = require('express-session');
+var moment = require('moment');
+var request = require('request');
+
+require('dotenv').load();
 var logger = require('morgan');
 var Grant = require('grant-express');
+var config = require('./config.json');
 
-var app = express();
-var grant = new Grant(require('./config.json'));
+var session = require('express-session');
+var pgDriver = require('connect-pg-simple')(session);
+
+var app = require('express')();
+
+var serverConnectionString = process.env.DATABASE_URL + "?ssl=true";
 
 app.use(logger('dev'));
-app.use(session({secret: 'ffffffffff90501960cb3b29615931aabf74201b'}));
+app.use(session({
+    store: new pgDriver({
+        conString: serverConnectionString
+    }),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 30 * 24 * 60 * 60 * 1000} // 30 days
+}));
+
+async function getHoursWorkedFromTimely(token) {
+
+}
+
+var grant = new Grant(config[process.env.NODE_ENV || 'development']);
+
+console.log(serverConnectionString);
+
 app.use(grant);
 
 app.get('/timely_callback', function (req, res) {
-    console.log(req.query);
-    res.end(JSON.stringify(req.query, null, 2))
+    req.session.timely = req.session.grant.response.access_token;
+    var destination = req.session.return;
+    delete req.session.return;
+
+    if (destination != null) {
+        res.redirect(destination);
+    }
+});
+
+app.get('/hours', function (req, res) {
+
+    if (req.session.timely == null) {
+        req.session.return = '/hours';
+        res.redirect('/connect/timely');
+    }
+
+    var token = req.session.timely;
+
+    var start = moment().startOf('month');
+    var end = moment().endOf('month');
+
+    var options = {
+        "url": "https://api.timelyapp.com/1.0/512361/reports",
+        "method": "POST",
+        "json": true,
+        "auth": {
+            "bearer": token
+        }
+        ,
+        "body": {
+            "estimated": false
+        }
+    };
+
+    console.log("token : " + token);
+
+    request(options, function (err, response, body) {
+        if (err) {
+            console.error("request failed : " + err);
+            res.send("error : " + err);
+            return;
+        }
+
+        var total = 0;
+
+        console.log(Object.keys(body));
+        console.log("is array " + Array.isArray(body));
+
+        res.send(JSON.stringify(body));
+        return;
+
+        if (body.length < 1) {
+            res.send("error data is not valid : " + JSON.stringify(body));
+            return;
+        }
+
+        body[0].projects.forEach(function (project) {
+            console.log(Object.keys(project));
+            console.log(project.project.name + " : " + project.logged_duration.formatted);
+            total += project.logged_duration.total_minutes;
+        });
+
+        res.send(JSON.stringify({"minutes": total}));
+    });
 });
 
 app.listen(3000, function () {
