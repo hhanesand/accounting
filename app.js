0ae16e58f302c28ec96fed0acec3fb524e65f4e1
Hakon Hanesand <hakon@abello.no>
2017-02-18 15:28:10 -0600

    Use eslint with standard

--- app.js
+++ app.js
@@ -1,106 +1,97 @@
-var moment = require('moment');
-var request = require('request');
+const moment = require('moment')
+const request = require('request')
 
-require('dotenv').load();
-var logger = require('morgan');
-var Grant = require('grant-express');
-var config = require('./config.json');
+require('dotenv').load()
+const logger = require('morgan')
+const Grant = require('grant-express')
+const config = require('./config.json')
 
-var session = require('express-session');
-var pgDriver = require('connect-pg-simple')(session);
+const session = require('express-session')
+const pgDriver = require('connect-pg-simple')(session)
 
-var app = require('express')();
+const app = require('express')()
 
-var serverConnectionString = process.env.DATABASE_URL + "?ssl=true";
+const serverConnectionString = process.env.DATABASE_URL + '?ssl=true'
 
-app.use(logger('dev'));
+app.use(logger('dev'))
 app.use(session({
-    store: new pgDriver({
-        conString: serverConnectionString
-    }),
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {maxAge: 30 * 24 * 60 * 60 * 1000} // 30 days
-}));
+  store: new pgDriver({
+    conString: serverConnectionString
+  }),
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 30 * 24 * 60 * 60 * 1000} // 30 days
+}))
 
-async function getHoursWorkedFromTimely(token) {
+async function getHoursWorkedFromTimely (token) {
 
 }
 
-var grant = new Grant(config[process.env.NODE_ENV || 'development']);
+const grant = new Grant(config[process.env.NODE_ENV || 'development'])
 
-console.log(serverConnectionString);
+console.log(serverConnectionString)
 
-app.use(grant);
+app.use(grant)
 
 app.get('/timely_callback', function (req, res) {
-    req.session.timely = req.session.grant.response.access_token;
-    var destination = req.session.return;
-    delete req.session.return;
+  req.session.timely = req.session.grant.response.access_token
+  const destination = req.session.return
+  delete req.session.return
 
-    if (destination != null) {
-        res.redirect(destination);
-    }
-});
+  if (destination != null) {
+    res.redirect(destination)
+  }
+})
 
 app.get('/hours', function (req, res) {
+  if (req.session.timely == null) {
+    req.session.return = '/hours'
+    res.redirect('/connect/timely')
+  }
 
-    if (req.session.timely == null) {
-        req.session.return = '/hours';
-        res.redirect('/connect/timely');
+  const options = {
+    'url': 'https://api.timelyapp.com/1.0/512361/reports',
+    'method': 'POST',
+    'json': true,
+    'auth': {
+      'bearer': req.session.timely
+    },
+    'body': {
+      'estimated': false
+    }
+  }
+
+  request(options, function (err, response, body) {
+    if (err) {
+      console.error('request failed : ' + err)
+      res.send('error : ' + err)
+      return
     }
 
-    var token = req.session.timely;
+    var total = 0
 
-    var start = moment().startOf('month');
-    var end = moment().endOf('month');
+    console.log(Object.keys(body))
+    console.log('is array ' + Array.isArray(body))
 
-    var options = {
-        "url": "https://api.timelyapp.com/1.0/512361/reports",
-        "method": "POST",
-        "json": true,
-        "auth": {
-            "bearer": token
-        }
-        ,
-        "body": {
-            "estimated": false
-        }
-    };
+    res.send(JSON.stringify(body))
+    return
 
-    console.log("token : " + token);
+    if (body.length < 1) {
+      res.send('error data is not valid : ' + JSON.stringify(body))
+      return
+    }
 
-    request(options, function (err, response, body) {
-        if (err) {
-            console.error("request failed : " + err);
-            res.send("error : " + err);
-            return;
-        }
+    body[0].projects.forEach(function (project) {
+      console.log(Object.keys(project))
+      console.log(project.project.name + ' : ' + project.logged_duration.formatted)
+      total += project.logged_duration.total_minutes
+    })
 
-        var total = 0;
-
-        console.log(Object.keys(body));
-        console.log("is array " + Array.isArray(body));
-
-        res.send(JSON.stringify(body));
-        return;
-
-        if (body.length < 1) {
-            res.send("error data is not valid : " + JSON.stringify(body));
-            return;
-        }
-
-        body[0].projects.forEach(function (project) {
-            console.log(Object.keys(project));
-            console.log(project.project.name + " : " + project.logged_duration.formatted);
-            total += project.logged_duration.total_minutes;
-        });
-
-        res.send(JSON.stringify({"minutes": total}));
-    });
-});
+    res.send(JSON.stringify({'minutes': total}))
+  })
+})
 
 app.listen(3000, function () {
-    console.log('Express server listening on port ' + 3000)
-});
+  console.log('Express server listening on port ' + 3000)
+})
