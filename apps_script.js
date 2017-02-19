fa62846196910a8035b408f07a71b11b36a3304b
Hakon Hanesand <hakon@abello.no>
2017-02-19 11:00:20 -0600

    Write to google sheets.

--- apps_script.js
+++ apps_script.js
@@ -2,14 +2,31 @@
  * Created by hakonhanesand on 2/18/17.
  */
 
+function copyFormatting (fromRange, toRange) {
+  'use strict'
+
+  fromRange.copyFormatToRange(toRange.getSheet(),
+    toRange.getColumn(),
+    toRange.getLastColumn(),
+    toRange.getRow(),
+    toRange.getLastRow())
+}
+
 function onOpen () {
   var response = UrlFetchApp.fetch('https://morning-dusk-92308.herokuapp.com/hours')
-
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
-  var earnings = spreadsheet.getSheetByName('Earnings')
 
-  var monthOffset = earnings.getRangeByName('monthOffsetHoursWorked').getValue()
-  var startHoursWorkedRange = earnings.getRangeByName('startHoursWorked')
+  if (response.getResponseCode() != 200) {
+    spreadsheet.toast('Could not fetch hours from timely...')
+    Logger.log('Error : Could not fetch hours from timely...')
+    return
+  }
 
-  startHoursWorkedRange.offset(monthOffset, 0).setValue(response.minutes)
+  var monthOffset = spreadsheet.getRangeByName('monthOffsetHoursWorked').getValue()
+  var startHoursWorkedRange = spreadsheet.getRangeByName('startHoursWorked')
+  var currentMonthHoursWorked = startHoursWorkedRange.offset(monthOffset, 0)
+
+  var responseJSON = JSON.parse(response.getContentText())
+  currentMonthHoursWorked.setValue(responseJSON.minutes * 0.000694444444444)
+  copyFormatting(startHoursWorkedRange, currentMonthHoursWorked)
 }
